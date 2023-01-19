b64a34cae5774be4f9b7da334452c35ead852baf
Paloma <palolili23@g.ucla.edu>
2023-01-19 15:45:37 -0800

    Descriptive info

---
+++ 01_R/01_descriptives.R
@@ -0,0 +1,25 @@
+## Descriptive statistics
+
+
+# Inclusion criteria - Flowchart ------------------------------------------
+
+
+# Number included into the study sample
+# Number excluded by dementia prevalence
+# Number excluded by stroke prevalence 
+# Check age distribution (prob will not include people 90 yo at baseline)
+# What descriptive characteristics do we want at baseline? Check nativity paper.
+
+# Table one ---------------------------------------------------------------
+
+
+
+# Exposure outcome counts -------------------------------------------------
+
+
+## ever vs. never stroke (to have an idea, not for exposure definition)
+## ever vs. never dementia (to have an idea, not for exposure definition)
+## deaths after stroke
+## death free of dementia dx. 
+
+
